8a083894f370ec70af4ed268f1aa546980b3d2b9
Alexander Fortin <shaftoe@users.noreply.github.com>
2025-10-27 20:41:18 -0400

    wip

--- internal/app/main.go
+++ internal/app/main.go
@@ -1,5 +1,15 @@
 package app
 
+import (
+	"fmt"
+	"time"
+)
+
+type Storage interface {
+	WriteRecord(record string, table string) error
+	ReadRecord(record string, table string) (string, error)
+}
+
 type Service struct{}
 
 func NewService() *Service {
@@ -12,4 +22,10 @@ func (s *Service) Greet(name string) string {
 	}
 
 	return name
+}
+
+func (s *Service) AddNewUser(email *string, storage Storage) error {
+	record := fmt.Sprintf(`{"email": "%s", "created_at": "%s"}`, *email, time.Now().Format(time.RFC3339))
+
+	return storage.WriteRecord(record, "api_keys")
 }
