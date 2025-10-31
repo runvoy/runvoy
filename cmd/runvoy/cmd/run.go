18d41b58828092a1e9f359cd9d585b83c83102e8
Alexander Fortin <shaftoe@users.noreply.github.com>
2025-10-30 20:54:10 -0400

    wip

--- cmd/runvoy/cmd/run.go
+++ cmd/runvoy/cmd/run.go
@@ -42,8 +42,6 @@ func runRun(cmd *cobra.Command, args []string) {
 	output.Success("Command execution started successfully")
 	output.KeyValue("Execution ID", resp.ExecutionID)
 	output.KeyValue("Status", resp.Status)
-	output.Info("Run 'runvoy logs %s' to view logs", output.Cyan(resp.ExecutionID))
 	output.Info("View logs in web viewer: %s?execution_id=%s",
-		output.Cyan(constants.WebviewerURL),
-		output.Cyan(resp.ExecutionID))
+		constants.WebviewerURL, output.Cyan(resp.ExecutionID))
 }
