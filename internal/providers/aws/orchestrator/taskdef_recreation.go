bad9c77529069ed16be2ef9e127cf978760aac8d
Alexander Fortin <shaftoe@users.noreply.github.com>
2025-11-15 21:40:42 -0400

    wip

--- internal/providers/aws/orchestrator/taskdef_recreation.go
+++
@@ -1,4 +0,0 @@
-// Package orchestrator previously contained reusable task definition recreation
-// logic for the health manager. That logic now lives in the ecsdefs package so
-// it can be reused without introducing circular dependencies between packages.
-package orchestrator
